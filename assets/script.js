f9af125d21d5ae944e782cb389c53d4bc46a72a4
eskukwa <easmsm@gmail.com>
2022-06-15 17:31:00 -0500

    added notes for future version

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,14 @@ var h1El = document.createElement("h1");
 var infoEl = document.createElement("div");
 var nameEl = document.createElement("div");
 var favoriteEl = document.createElement("div");
-
+var listEl = document.createElement("ul");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+var li5 = document.createElement("li");
 let btn = document.createElement("button");
+
 btn.innerHTML = "Generate";
 document.body.appendChild(btn);
 
@@ -13,13 +19,6 @@ document.body.appendChild(btn);
 var generateBtn = document.querySelector("#generate");
 
 //to make the list of variables/password length to select for the password, selections are going to need to be sotred in local storage
-var listEl = document.createElement("ul");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-var li5 = document.createElement("li");
-
 h1El.textContent = "Please select your password requirements:";
 li1.textContent = "Password Length";
 li2.textContent = "Upper Case Letters";
@@ -27,13 +26,13 @@ li3.textContent = "Lower Case Letters";
 li4.textContent = "Numbers";
 li5.textContent = "Special Characters";
 
-
+favoriteEl.appendChild(listEl);
 listEl.appendChild(li1);
 listEl.appendChild(li2);
 listEl.appendChild(li3);
 listEl.appendChild(li4);
 listEl.appendChild(li5);
-favoriteEl.appendChild(listEl);
+
 
 /*localStorage.setItem(); //store chosen password length
 localStorage.setItem(); //store if/not uppercase
@@ -63,6 +62,13 @@ function generatePassword() {
 
       // special characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" (between double quotes)
     for (let i = 1; i <= 8; i++) {
+
+      /*
+      var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      var lowerChars = "abcdefghijklmnopqrstuvwxyz"
+      var numberChars = "0123456789"
+      var specialChars = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+      */
         var char = Math.floor(Math.random()
                     * str.length + 1);
           
