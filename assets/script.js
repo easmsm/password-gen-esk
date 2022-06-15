b1e9217c68d697044a40122e1786321ab854abf3
eskukwa <easmsm@gmail.com>
2022-06-15 07:50:18 -0400

    added for loop to generate password with str, char, and math.floor

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
 
+let btn = document.createElement("button");
+btn.innerHTML = "Generate";
+document.body.appendChild(btn);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,10 +11,34 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
+
+/*function gfg_Run() {
+  el_down.innerHTML = generateBtn();
+}
+
+var el_down = document.getElementById("generate"); */
+
+//Function to generate combination of password
+
+function generatePassword() {
+    var password = '';
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
+            'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+      
+    for (let i = 1; i <= 8; i++) {
+        var char = Math.floor(Math.random()
+                    * str.length + 1);
+          
+        password += str.charAt(char)
+    }
+      
+    return password;
+}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+document.getElementById("generate").addEventListener("click", generatePassword);
+
+writePassword();
